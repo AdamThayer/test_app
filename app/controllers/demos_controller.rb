8bdd2bab2838cf26b5e978cf50e68e3231ccf347
Adam Thayer <adamjthayer@gmail.com>
2016-04-25 15:53:22 -0700

    You are done making changes

--- app/controllers/demos_controller.rb
+++ app/controllers/demos_controller.rb
@@ -1,5 +1,10 @@
 class DemosController < ApllicationController
   def changes
-    # This is me making some changes
+    # This is you making some changes to the code in the same area as I am.
+    # You are also adding way more than I am
+
+
+
+    # Wow that was a lot.
   end
 end
