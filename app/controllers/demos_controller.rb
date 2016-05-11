ebe53c6cea104f9e76554cc64bca082c3424eb4b
Adam Thayer <adamjthayer@gmail.com>
2016-05-11 09:03:51 -0700

    Making a commit

--- app/controllers/demos_controller.rb
+++ app/controllers/demos_controller.rb
@@ -1,10 +1,7 @@
 class DemosController < ApllicationController
+
   def changes
 
-    # This is you making some changes to the code in the same area as I am.
-    # You are also adding way more than I am
-
-    # This is me making some changes and I am going to add some more here.
-
   end
+
 end
