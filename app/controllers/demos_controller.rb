e6e37b642d7ddf261d673ee37b951860343e79dd
Adam Thayer <adamjthayer@gmail.com>
2016-06-28 13:43:48 -0700

    The user was victorious

--- app/controllers/demos_controller.rb
+++ app/controllers/demos_controller.rb
@@ -2,6 +2,7 @@ class DemosController < ApllicationController
 
   def changes
     # "I'm not a program, I am a User" Sam Flynn
+    # "You can't steal something that's designed to be given away"
   end
 
 end
